65f4991c6f79ed5b55f95293238103f611893b75
Volt <sonianadashkevich@gmail.com>
2019-11-22 08:40:44 +0100

    zadanie1 poprawione

--- src/Competition.java
+++ src/Competition.java
@@ -3,37 +3,41 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.TreeSet;
 
 public class Competition {
     public static void main(String[] args) {
-        ResultComparator comparator = new ResultComparator();
-        TreeSet<Sportsman> sportsmen = new TreeSet<>(comparator);
-        sortAndRecord(sportsmen);
-    }
-
-    private static void sortAndRecord(TreeSet<Sportsman> sportsmen) {
-        try (BufferedWriter bfw = new BufferedWriter(new FileWriter(new File("competition.csv")))) {
-            System.out.println("Podaj wynik kolejnego gracza (lub stop)");
-            checkInput(sportsmen, bfw);
+        try {
+            checkInput();
         } catch (IOException e) {
-            System.out.println(e.getMessage());
+            e.printStackTrace();
         }
     }
 
-    private static void checkInput(TreeSet<Sportsman> sportsmen, BufferedWriter bfw) throws IOException {
-        Scanner input = new Scanner(System.in);
+    private static void checkInput() throws IOException {
+        System.out.println("Podaj wynik kolejnego gracza (lub stop)");
+        try (BufferedWriter bfw = new BufferedWriter(new FileWriter(new File("competition.csv")))) {
+            Scanner input = new Scanner(System.in);
+            ResultComparator comparator = new ResultComparator();
+            inputAndRecord(bfw, input,comparator);
+        }
+    }
+
+    private static void inputAndRecord(BufferedWriter bfw, Scanner input, ResultComparator comparator) throws IOException {
         String line = input.nextLine();
+        Set<Sportsman> sportsmen = new TreeSet<>(comparator);
         while (!line.equals("")) {
             if (line.equals("stop")) {
                 input.close();
                 for (Sportsman s : sportsmen) {
                     bfw.write(s.toString());
+                    bfw.newLine();
                 }
                 break;
             } else {
-                sportsmen.add(new Sportsman(line, input.nextLine(), input.nextDouble()));
-                input.nextLine();
+                String[] results = line.split(" ");
+                sportsmen.add(new Sportsman(results[0],results[1],Integer.parseInt(results[2])));
                 System.out.println("Podaj wynik kolejnego gracza (lub stop)");
                 line = input.nextLine();
             }
